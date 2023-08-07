7dc3e8395dd8a338c5e131e1fa3f13202e56e36c
Jurimeu <139811045+Jurimeu@users.noreply.github.com>
2023-08-07 09:28:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function verificarNumero() {
+      var numero = document.getElementById("numero").value;
+      var resultadoElement = document.getElementById("resultado");
 
+      if (numero  >= 18) {
+        resultadoElement.textContent = "Sinto lhe informa, mas aprende isso depois de adulto é complicado...";
+      } 
+      
+      else if (numero < 18){
+        resultadoElement.textContent = "Esta no caminho certo";
+      }
+
+else if (numero  >= 100) {
+        resultadoElement.textContent = "Volta pra tumba mumia pilantra";
+      } 
+	      
+ else if (numero < 1){
+        resultadoElement.textContent = "Nem nasceu ainda K7";
+      }
+	      
+ else if (numero < 4){
+        resultadoElement.textContent = "novo D+";
+      }
+	      
+      else {  resultadoElement.textContent = "So numeros animal...";}
+	    
+	     event.preventDefault();
+}
